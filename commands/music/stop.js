4284a81e23f2954ccf764da0116f3c10500908b3
Sherif Salama <36298707+Artisians@users.noreply.github.com>
2021-01-07 05:50:45 +0200

    Play, Stop and Queue added!
    ?play URL with queue support has been added
    ?stop has been added

---
+++ commands/music/stop.js
@@ -0,0 +1,23 @@
+module.exports = {
+  commands: ["stop"],
+  minArgs: 0,
+  requiredRoles: ["Member"],
+  callback: async (message) => {
+    const voiceChannel = message.member.voice.channel;
+    const Discord = require("discord.js");
+
+    if (!voiceChannel) {
+      return message.channel.send(
+        "You must be in a voice channel to stop the music!"
+      );
+    }
+    voiceChannel.leave();
+    const leftVoiceConfirmationEmbed = new Discord.MessageEmbed()
+      .setColor("GREEN")
+      .setDescription(`✅ Audio stopped!`);
+    let leftVoiceConfirmation = await message.channel.send(
+      leftVoiceConfirmationEmbed
+    );
+    leftVoiceConfirmation.delete();
+  },
+};
