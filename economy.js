4fff0beb958c52e3a63045adc79c296b48915f73
BiXtico <56024802+BiXtico@users.noreply.github.com>
2021-01-09 01:22:50 +0200

    update with balance command

---
+++ economy.js
@@ -0,0 +1,33 @@
+const mongo = require('./mongo')
+const profileSchema = require('./schemas/profile-schema')
+
+//module.exports = (client) =>{}
+
+module.exports.getCoins = async (guildId,userId) =>{
+    return await mongo().then(async mongoose => {
+        try{
+            //visualizing the change in the database
+            console.log('Running findOne()')
+            const result = await profileSchema.findOne({
+                guildId,
+                userId,
+            })
+            console.log('RESULT:', result)
+
+            let coins =0
+            if(result){
+                coins = result.coins
+            } else {
+                console.log('Inserting a document')
+                await new profileSchema({
+                    guildId,
+                    userId,
+                    coins,
+                }).save()
+            }
+            return coins
+        }finally{
+            mongoose.connection.close()
+        }
+    })
+}
