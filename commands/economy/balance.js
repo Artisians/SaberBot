f41ad88ee0882702409f0f4df6ab829908fd28c0
BiXtico <56024802+BiXtico@users.noreply.github.com>
2021-01-09 14:41:17 +0200

    Balance command and addbalance command, economy system update

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -1,20 +1,18 @@
 const economy = require('../../economy')
 
 module.exports = {
-    //to get the balance of a character by writing balance or bal
-    commands : ['balance', 'bal'],
-    maxArgs: 1, 
-    // to get the balance with bal or bal @user (for a certain user)
-    expectedArgs: "[Target user's @]",
-    callback: async (message) => {
-        // target identifying after the command chacking if it's for another useer or the autho
-        const target = message.mentions.users.first() || message.author
-        const targetId = target.id
+  commands: ['balance', 'bal'],
+  maxArgs: 1,
+  expectedArgs: "[Target user's @]",
+  callback: async (message) => {
+    const target = message.mentions.users.first() || message.author
+    const targetId = target.id
 
-        const guildId = message.guild.id
-        const userId = target.id
-        const coins = await economy.getCoins(guildId,userId)
-        message.reply(`That user has  ${coins} coins`)
-    },
-    
+    const guildId = message.guild.id
+    const userId = target.id
+
+    const coins = await economy.getCoins(guildId, userId)
+
+    message.reply(`That user has ${coins} coins!`)
+  },
 }
